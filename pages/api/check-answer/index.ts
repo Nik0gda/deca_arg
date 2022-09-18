e3e979579b855878a3741d4b8077d69afae00b18
Nik0gda <cebanu.dan3@gmail.com>
2022-09-18 05:42:22 -0700

    Gallery URL fix no.1

--- pages/api/check-answer/index.ts
+++ pages/api/check-answer/index.ts
@@ -6,18 +6,39 @@ export default async function handler(
 ) {
   let { guesses }: { guesses: string[][] } = req.body;
   const answers = [
-    ['2009Block0', 'https://deca.art/sq/teztheszn/3gKAyEDO'],
-    ['waleedshah', 'https://deca.art/sq/teztheszn/LgjA6OMo'],
-    ['hilbertspace', 'https://deca.art/sq/teztheszn/mDXnyVg0'],
-    ['Deca', 'https://deca.art/sq/teztheszn/0xA083g8'],
-    ['NiftyFifty', 'https://deca.art/sq/teztheszn/0MwzWeDV'],
+    [
+      '2009Block0',
+      'https://deca.art/sq/teztheszn/3gKAyEDO',
+      'xxxxxxxxxxxxxxxxxxrxxxxxxxxxxxxxx',
+    ],
+    [
+      'waleedshah',
+      'https://deca.art/sq/teztheszn/LgjA6OMo',
+      'https://deca.art/sq/teztheszn/pMYLBEDZ',
+    ],
+    [
+      'hilbertspace',
+      'https://deca.art/sq/teztheszn/mDXnyVg0',
+      'xxxxxxxxxxxxxxxxxxxgxxxxxxxgxxxxxx',
+    ],
+    [
+      'Deca',
+      'https://deca.art/sq/teztheszn/0xA083g8',
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxwexxxx',
+    ],
+    [
+      'NiftyFifty',
+      'https://deca.art/sq/teztheszn/0MwzWeDV',
+      'xxxxxxxxxxxxxxxxxxxxxxxxfdsxxxxxxxx',
+    ],
   ];
 
   let correct = [false, false, false, false, false];
 
   for (let i = 0; i < 5; i++) {
     if (
-      guesses[i][0].toLowerCase() === answers[i][0].toLowerCase() &&
+      (guesses[i][0].toLowerCase() === answers[i][0].toLowerCase() ||
+        guesses[i][0].toLowerCase() === answers[i][2]) &&
       guesses[i][1].toLowerCase() === answers[i][1].toLowerCase()
     )
       correct[i] = true;
